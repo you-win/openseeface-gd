2949268338197ed375b471e3992bd1a9dd4b6daa
Tim Yuen <timothy.yuen22@gmail.com>
2023-01-20 18:50:31 -0500

    finish adding all possible osf options

--- config.gd
+++ config.gd
@@ -26,12 +26,78 @@ var ip := ""
 @export
 var port := ""
 @export
+var list_cameras := ""
+@export
+var list_dcaps := ""
+@export
 var width := ""
 @export
 var height := ""
+@export
+var dcap := ""
+@export
+var blackmagic := ""
 @export
 var fps := ""
 @export
 var capture := ""
 @export
 var mirror_input := false
+@export
+var max_threads := ""
+@export
+var threshold := ""
+@export
+var detection_threshold := ""
+@export
+var visualize := ""
+@export
+var pnp_points := ""
+@export
+var silent := ""
+@export
+var faces := ""
+@export
+var scan_retinaface := ""
+@export
+var scan_every := ""
+@export
+var discard_after := ""
+@export
+var max_feature_updates := ""
+@export
+var no_3d_adapt := ""
+@export
+var try_hard := ""
+@export
+var video_out := ""
+@export
+var video_scale := ""
+@export
+var video_fps := ""
+@export
+var raw_rgb := ""
+@export
+var log_data := ""
+@export
+var log_output := ""
+@export
+var model := ""
+@export
+var model_dir := ""
+@export
+var gaze_tracking := ""
+@export
+var face_id_offset := ""
+@export
+var repeat_video := ""
+@export
+var dump_points := ""
+@export
+var benchmark := ""
+@export
+var use_dshowcapture := ""
+@export
+var blackmagic_options := ""
+@export
+var priority := ""
--- main.gd
+++ main.gd
@@ -8,19 +8,19 @@ const OSF_OPTIONS := {
 		"display_name": "IP",
 		"flag": "--ip",
 		"config_mapping": "ip",
-		"hint": "The IP address for sending tracking data"
+		"hint": "The IP address for sending tracking data."
 	},
 	"Port": {
 		"display_name": "Port",
 		"flag": "--port",
 		"config_mapping": "port",
-		"hint": "The port for sending tracking data"
+		"hint": "The port for sending tracking data."
 	},
 	"List Cameras": {
 		"display_name": "List Cameras",
 		"flag": "--list-cameras",
 		"config_mapping": "list_cameras",
-		"hint": "Set to 1 to list the available cameras and quit. Set to 2 or higher to output only the camera names",
+		"hint": "Set to 1 to list the available cameras and quit. Set to 2 or higher to output only the camera names.",
 		"windows_only": true
 	},
 	"List DCaps": {
@@ -41,117 +41,195 @@ const OSF_OPTIONS := {
 		"display_name": "Blackmagic",
 		"flag": "--blackmagic",
 		"config_mapping": "blackmagic",
-		"hint": "Set to 1 to enable Blackmagic device support, if applicable",
+		"hint": "Set to 1 to enable Blackmagic device support, if applicable.",
 		"windows_only": true
 	},
 	"Width": {
 		"display_name": "Width",
 		"flag": "--width",
 		"config_mapping": "width",
-		"hint": "The RGB width"
+		"hint": "The RGB width."
 	},
 	"Height": {
 		"display_name": "Height",
 		"flag": "--height",
 		"config_mapping": "height",
-		"hint": "The raw RGB height"
+		"hint": "The raw RGB height."
 	},
 	"FPS": {
 		"display_name": "FPS",
 		"flag": "--fps",
 		"config_mapping": "fps",
-		"hint": "The capture frames per second"
+		"hint": "The capture frames per second."
 	},
 	"Capture": {
 		"display_name": "Capture",
 		"flag": "--capture",
 		"config_mapping": "capture",
-		"hint": "The camera to use (e.g. Windows: 0, 1 Linux: /dev/video0)"
+		"hint": "The camera to use (e.g. Windows: 0, 1 Linux: /dev/video0)."
 	},
 	"Mirror Input": {
 		"display_name": "Mirror Input",
 		"flag": "--mirror-input",
 		"config_mapping": "mirror_input",
-		"hint": "If the input should be mirrored before processing",
+		"hint": "If the input should be mirrored before processing.",
 		"type": TYPE_BOOL
 	},
 	"Max Threads": {
-		
+		"display_name": "Max Threads",
+		"flag": "--max-threads",
+		"config_mapping": "max_threads",
+		"hint": "The maximum number of threads to use. MORE THREADS DOES _NOT_ MEAN MORE PERFORMANCE."
 	},
 	"Threshold": {
-		
+		"display_name": "Threshold",
+		"flag": "--threshold",
+		"config_mapping": "threshold",
+		"hint": "The minimum confidence threshold for face tracking."
 	},
 	"Detection Threshold": {
-		
+		"display_name": "Detection Threshold",
+		"flag": "--detection-threshold",
+		"config_mapping": "detection_threshold",
+		"hint": "The minimum confidence interval for face detection."
 	},
 	"Visualize": {
-		
+		"display_name": "Visualize",
+		"flag": "--visualize",
+		"config_mapping": "visualize",
+		"hint": "Set to 1 to visualize the tracking results, set to 2 to also show face ids, set to 3 to add confidence values, and set to 4 to add numbers to the point display."
 	},
 	"PnP Points": {
-		
+		"display_name": "PnP Points",
+		"flag": "--pnp-points",
+		"config_mapping": "pnp_points",
+		"hint": "Set to 1 to add the 3D fitting points to the visualization."
 	},
 	"Silent": {
-		
+		"display_name": "Silent",
+		"flag": "--silent",
+		"config_mapping": "silent",
+		"hint": "Set to 1 to prevent console text output."
 	},
 	"Faces": {
-		
+		"display_name": "Faces",
+		"flag": "--faces",
+		"config_mapping": "faces",
+		"hint": "Set the maximum number of faces (slow)."
 	},
 	"Scan RetinaFace": {
-		
+		"display_name": "Scan RetinaFace",
+		"flag": "--scan-retinaface",
+		"config_mapping": "scan_retinaface",
+		"hint": "Set to 1 to enable scanning for additional faces using RetinaFace in a background thread. Otherwise, a simpler, faster face detection mechanism is used. Does nothing when the maximum number of faces is 1."
 	},
 	"Scan Every": {
-		
+		"display_name": "Scan Every",
+		"flag": "--scan-every",
+		"config_mapping": "scan_every",
+		"hint": "The amount of frames to wait before scanning for new faces."
 	},
 	"Discard After": {
-		
+		"display_name": "Discard After",
+		"flag": "--discard-after",
+		"config_mapping": "discard_after",
+		"hint": "How long the tracker should keep looking for lost faces."
 	},
 	"Max Feature Updates": {
-		
+		"display_name": "Max Feature Updates",
+		"flag": "--max-feature-updates",
+		"config_mapping": "max_feature_updates",
+		"hint": "The number of seconds after which feature min/max/medium values will no longer be updated once a face has been detected."
 	},
 	"No 3D Adapt": {
-		
+		"display_name": "No 3D Adapt",
+		"flag": "--no-3d-adapt",
+		"config_mapping": "no_3d_adapt",
+		"hint": "When set to 1, the 3D face model will not be adapted to increase the fit."
 	},
 	"Try Hard": {
-		
+		"display_name": "Try Hard",
+		"flag": "--try-hard",
+		"config_mapping": "try_hard",
+		"hint": "When set to 1, the tracker will try harder to find a face."
 	},
 	"Video Out": {
-		
+		"display_name": "Video Out",
+		"flag": "--video-out",
+		"config_mapping": "video_out",
+		"hint": "The filename of an AVI file to save the tracking visualization as a video."
 	},
 	"Video Scale": {
-		
+		"display_name": "Video Scale",
+		"flag": "--video-scale",
+		"config_mapping": "video_scale",
+		"hint": "The resolution scale factor applied to the saved AVI file. Choices: 1, 2, 3, 4."
 	},
 	"Video FPS": {
-		
+		"display_name": "Video FPS",
+		"flag": "--video-fps",
+		"config_mapping": "video_fps",
+		"hint": "The frame rate of the output AVI file."
 	},
 	"Raw RGB": {
-		
+		"display_name": "Raw RGB",
+		"flag": "--raw-rgb",
+		"config_mapping": "raw_rgb",
+		"hint": "When set, raw RGB frames of the size given from Width and Height are read from standard input instead of reading a video."
 	},
 	"Log Data": {
-		
+		"display_name": "Log Data",
+		"flag": "--log-data",
+		"config_mapping": "log_data",
+		"hint": "A filename where tracking data will be logged."
 	},
 	"Log Output": {
-		
+		"display_name": "Log Output",
+		"flag": "--log-output",
+		"config_mapping": "log_output",
+		"hint": "A filename where console data will be logged."
 	},
 	"Model": {
-		
+		"display_name": "Model",
+		"flag": "--model",
+		"config_mapping": "model",
+		"hint": "The tracking model to use. Higher numbers are models with better tracking quality but slower speed except for model 4 which is wink optimized. Models 1 and 0 tend to be too rigid for expression and blink detection. Model -2 is roughly equivalent to model 1 but faster. Model -3 is between models 0 and -1."
 	},
 	"Model Directory": {
-		
+		"display_name": "Model Directory",
+		"flag": "--model-dir",
+		"config_mapping": "model_dir",
+		"hint": "The path to the directory containing the .onnx model files."
 	},
 	"Gaze Tracking": {
-		
+		"display_name": "Gaze Tracking",
+		"flag": "--gaze-tracking",
+		"config_mapping": "gaze_tracking",
+		"hint": "Set to 1 to enable gaze tracking which makes things slightly slower."
 	},
 	"Face ID Offset": {
-		
+		"display_name": "Face ID Offset",
+		"flag": "--face-id-offset",
+		"config_mapping": "face_id_offset",
+		"hint": "An offset added to all face IDs, which can be useful for mixing tracking data from multiple network sources."
 	},
 	"Repeat Video": {
-		
+		"display_name": "Repeat Video",
+		"flag": "--repeat-video",
+		"config_mapping": "repeat_video",
+		"hint": "When set to 1 and a video file was specified with Capture, the tracker will loop until interrupted."
 	},
 	"Dump Points": {
-		
+		"display_name": "Dump Points",
+		"flag": "--dump-points",
+		"config_mapping": "dump_points",
+		"hint": "When set to a filename, the current face 3D points are made symmetric and dumped to the given file when quitting the visualization with the `q` key."
 	},
 	"Benchmark": {
-		
+		"display_name": "Benchmark",
+		"flag": "--benchmark",
+		"config_mapping": "benchmark",
+		"hint": "Set to 1 to benchmark the various tracking models starting with the best and ending with the fastest. Gaze tracking is disabled for models with negative IDs."
 	},
 	"Use DShowCapture": {
 		"display_name": "Use DShowCapture",
@@ -217,6 +295,7 @@ class OsfOption extends HBoxContainer:
 		return _element.text if _element is LineEdit else _element.button_pressed
 
 var _config: Config = null
+var _is_windows := false
 
 @onready
 var _status := %Status as RichTextLabel
@@ -237,9 +316,11 @@ var _osf_options := %OsfOptions as VBoxContainer
 #-----------------------------------------------------------------------------#
 
 func _ready() -> void:
+	get_viewport().gui_embed_subwindows = false
+	
 	_config = _read_config()
 	
-	get_viewport().gui_embed_subwindows = false
+	_is_windows = OS.get_name().to_lower() == "windows"
 	
 	var run_button := %Run as Button
 	run_button.pressed.connect(func() -> void:
@@ -247,7 +328,11 @@ func _ready() -> void:
 		for child in _osf_options.get_children():
 			run_options.push_back(OSF_OPTIONS[child.option_name()].flag)
 			run_options.push_back(child.option_value())
-			pass
+		
+		var exe_path := ""
+		# TODO stub, check which run type is selected and construct the path
+		
+		OS.create_process(exe_path, run_options, true)
 	)
 	
 	var binary_options := %BinaryOptions as VBoxContainer
@@ -287,6 +372,8 @@ func _ready() -> void:
 	)
 	
 	for data in OSF_OPTIONS.values():
+		if data.get("windows_only", false) and not _is_windows:
+			continue
 		var option := OsfOption.new(
 			data.display_name,
 			_config.get(data.config_mapping),
